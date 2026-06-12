e8a2937965ec8f8d2f96605054f660c92b130e1c
Martin Kjems <mkjems@gmail.com>
2026-06-12 23:12:33 +0200

    update CACHE_NAME

--- client/sw.js
+++ client/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gunfight-v18';
+const CACHE_NAME = 'gunfight-v19';
 const LOCAL_DEVELOPMENT_HOSTS = ['localhost', '127.0.0.1'];
 const IS_LOCAL_DEVELOPMENT =
     LOCAL_DEVELOPMENT_HOSTS.indexOf(self.location.hostname) >= 0;
